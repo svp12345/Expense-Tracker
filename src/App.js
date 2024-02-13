57b686e3076f15232274b382f9deb6c21fd8b63a
Shruti Pokale <64649386+svp12345@users.noreply.github.com>
2024-02-13 20:12:34 +0530

    files are added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer";
+import Header from "./components/Header";
+import AddExpense from "./pages/add-expense";
+import Home from "./pages/Home";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" exact element={<Home/>}></Route>
+        <Route path="/add-expense" element={<AddExpense/>}></Route>
+      </Routes>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
